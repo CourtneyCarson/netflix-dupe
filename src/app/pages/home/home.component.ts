e979d4bbbc27d0babdc13f3b8978dd4d289df604
Courtney Carson <cmacarson@gmail.com>
2023-10-10 18:13:05 -0700

    refactor(movie service): create movie service, and get data :D
    create movie service, and get data :D

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private movieService: MovieApiServiceService) {}
 
   ngOnInit(): void {
+    this.bannerMovieData();
   }
 
+  // banner data
+  bannerMovieData() {
+    this.movieService.bannerApiData().subscribe((data) => {
+      console.log('movie data :D', data);
+    });
+  }
 }
---
+++ src/app/service/movie-api-service.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieApiServiceService {
+  constructor(private http: HttpClient) {}
+  BASE_URL = 'https://api.themoviedb.org/3';
+  API_KEY = '08cc33bd5ae3a747598ce2ad84376e66';
+
+  bannerApiData(): Observable<any> {
+    return this.http.get(
+      ` ${this.BASE_URL}/trending/all/week?api_key=${this.API_KEY}&language=en-US`
+    );
+  }
+}
